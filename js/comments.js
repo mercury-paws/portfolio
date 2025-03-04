9cd214fd5a0d2a5695ff986a2a77d00558344c4c
Anastasiia Kutsenko <145927427+mercury-paws@users.noreply.github.com>
2025-03-04 10:29:26 +0100

    localComments

--- js/comments.js
+++ js/comments.js
@@ -3,6 +3,28 @@
 const receivedComment = document.querySelector(".received-comment")
 const prev = document.querySelector(".prev")
 const next = document.querySelector(".next")
+const localComments = [
+  {
+    name: "Sebastian",
+    comment:
+      "Working with you was an absolute pleasure! The process was smooth and easy. Thank you for teamwork and for building great page!",
+  },
+  {
+    name: "Юлія",
+    comment:
+      "Дуже добре попрацювали разом над проектом. Завжди допомагала колегам, якщо виникали якісь питання, роботу робила вчасно. Дякую!",
+  },
+  {
+    name: "Natalia",
+    comment:
+      "Working with you felt like having a deep conversation with someone who understands human and the need for education. Thanks a lot!",
+  },
+  {
+    name: "Helen",
+    comment:
+      "I couldn't be happier with the result! Your expertise and innovative approach made the entire experience smooth and rewarding.",
+  },
+];
 
 axios.defaults.baseURL = "https://profile-server-qbyd.onrender.com";
 axios.defaults.withCredentials = true;
@@ -15,21 +37,34 @@ async function fetchComments() {
     const response = await axios.get("/comments");
 	return response.data.data.data;
   } catch (error) {
-    throw new Error(`Error fetching comments: ${error.response?.data?.message || error.message}`);
+    console.warn("Server request failed. Using local comments.json instead.");
+    return localComments;
   }  
 }
 
 async function renderComments() {
   let comments = await fetchComments();
-  const { name, comment } = comments[index];
+
+  let markup;
+
+  if (comments) {
+    const { name, comment } = comments[index];
 
   const sanitizedComment = DOMPurify.sanitize(comment);
   const sanitizedName = DOMPurify.sanitize(name);
 
-    const markup = `
+  markup = `
     <p class="client-text-review">${sanitizedComment}</p>
     <p class="client-name">${sanitizedName}</p>
   `;
+  } else {
+   
+    markup = `
+    <p class="client-text-review">${sanitizedComment}</p>
+    <p class="client-name">${sanitizedName}</p>
+  `;
+  }
+  
 
     receivedComment.innerHTML = markup;
     prev.disabled = index === 0;
